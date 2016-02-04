2e4e90eb46aa61c3e7ad4037b52402ed66bf2181
Sam Brauer <sbrauer@rentpath.com>
2016-02-04 15:15:09 -0500

    [SB][110335796] Access helper methods via #manifest
    Since helper methods are combined into one namespace, we encountered
    collisions in a Rails app with various helper methods from
    ActionView::Helpers::AssetUrlHelper (asset_path, asset_url, etc).
    
    With this change we expose a single helper method #manifest which
    returns an object that responds to the original helper methods
    (#asset_url, etc).

--- lib/asset_manifest_helper/helpers.rb
+++ lib/asset_manifest_helper/helpers.rb
@@ -1,35 +1,45 @@
 module AssetManifestHelper
-  def asset_url(asset)
-    url_for_path(asset_path(asset))
+  def manifest
+    @manifest ||= Manifest.new
   end
 
-  def style_url(bundle)
-    url_for_path(style_path(bundle))
-  end
+  class Manifest
+    def asset_url(asset)
+      url_for_path(asset_path(asset))
+    end
 
-  def script_url(bundle)
-    url_for_path(script_path(bundle))
-  end
+    def style_url(bundle)
+      url_for_path(style_path(bundle))
+    end
 
-  def asset_path(asset)
-    AssetManifestHelper.configuration.asset_manifest.fetch(asset, asset)
-  end
+    def script_url(bundle)
+      url_for_path(script_path(bundle))
+    end
 
-  def style_path(bundle)
-    AssetManifestHelper.configuration.bundle_manifest.fetch(bundle, {})['css']
-  end
+    def asset_path(asset)
+      configuration.asset_manifest.fetch(asset, asset)
+    end
 
-  def script_path(bundle)
-    AssetManifestHelper.configuration.bundle_manifest.fetch(bundle, {})['js']
-  end
+    def style_path(bundle)
+      configuration.bundle_manifest.fetch(bundle, {})['css']
+    end
 
-  def url_for_path(path)
-    return nil unless path
-    separator = path.start_with?('/') ? '' : '/'
-    "#{asset_root_url}#{separator}#{path}"
-  end
+    def script_path(bundle)
+      configuration.bundle_manifest.fetch(bundle, {})['js']
+    end
 
-  def asset_root_url
-    "#{AssetManifestHelper.configuration.protocol}://#{AssetManifestHelper.configuration.domain}"
+    def url_for_path(path)
+      return nil unless path
+      separator = path.start_with?('/') ? '' : '/'
+      "#{asset_root_url}#{separator}#{path}"
+    end
+
+    def asset_root_url
+      "#{configuration.protocol}://#{configuration.domain}"
+    end
+
+    def configuration
+      AssetManifestHelper.configuration
+    end
   end
 end
